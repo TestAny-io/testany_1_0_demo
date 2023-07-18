14e7b0e5781a5023cb7d7d09c930552786b75db8
boyi <boyi.liang@testany.io>
2023-07-18 17:08:15 +0800

    add crud secrets sample code

---
+++ python_tf_demo/util/get_secret_value.py
@@ -0,0 +1,32 @@
+import requests
+import boto3
+import botocore
+import json
+import os
+import sys
+
+
+def get_secret(client, secret_name):
+    response = client.get_secret_value(
+        SecretId=secret_name
+    )
+
+    if 'SecretString' in response:
+        secret_value = response['SecretString']
+    else:
+        # If the secret is stored as a binary value
+        secret_value = response['SecretBinary']
+
+    return secret_value
+
+input_param=sys.argv[1]
+secret_name_prefix = 'Ford/apiAuto/'
+secret_name=secret_name_prefix+input_param
+
+# Create a session using the environment variables
+session = boto3.Session()
+
+secrets_manager_client = session.client(service_name='secretsmanager',region_name='cn-northwest-1')
+
+retrieved_secret = get_secret(secrets_manager_client, secret_name)
+print(retrieved_secret)
